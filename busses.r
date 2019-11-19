04e02946620c05d264eba6a45c3446ae28195c86
Shhh <shanwilkinson2@users.noreply.github.com>
2019-11-19 21:21:28 +0000

    small changes

--- busses.r
+++ busses.r
@@ -31,39 +31,10 @@ library(purrr)
 # stop times - big file 8MB
   stop_times <- read_csv("https://www.dropbox.com/sh/4djlyzcdo0ytpcf/AADqaMDXl8jsJJSUongzQiwqa/gtdf-out/stop_times.txt?dl=1")
  
-########## 
+e########## 
 # which routes run to which stops?  
   
-#   stops_short <- stops %>%
-#     st_drop_geometry() %>%
-#     select(stop_id, stop_name) %>%
-#     left_join()
-#   
-#  # stop_trip <- stop_times %>%
-#  #    group_by(stop_id, trip_id) %>%
-#  #    summarise(n()) %>%
-#  #    select(stop_id, trip_id)
-#   
-#   # unique combinations of stop & trip 
-#   stop_trip <- stop_times %>%
-#     selet(-arrival_time, -departure_time) %>%
-#     unique(stop_times[,c("stop_id", "trip_id")]) 
-#  
-#   
-#   trips %>%
-#     group_by(service_id) %>%
-#     summarise(num_trips = n())
-#     
-#   nrow(unique(trips[,c("service_id", "trip_id")]))
-#    
-# trip_stop <- left_join(trips, stop_route)  
-#   
-#     
-# stop_route <- stops %>%
-#   st_drop_geometry() %>%
-#   select(stop_id) %>%
-#   left_join(stop_trip, by = )
- 
+
 ### 
 
 # # add stop location to stop times - takes too long
@@ -74,14 +45,21 @@ library(purrr)
   # stop_times <- left_join(stop_times, trips, by = "trip_id")
   stop_times <- stop_times %>%
     group_by(trip_id) %>%
-    nest()
-  stop_times <- left_join(stop_times, trips, by = "trip_id")
-  stop_times <- left_join(stop_times, routes, by = "route_id" )
+    nest() %>%
+  left_join(trips, by = "trip_id") %>%
+  left_join(routes, by = "route_id")
+
+stop_times <- stop_times %>%
+  group_by(route_id) %>%
+  filter(trip_id == min(trip_id)) %>%
   
-#   stop_times <- stop_times %>%
-#     nest(-route_id, -service_id, -trip_headsign)
-#   # service_id seems to be a code version of route_id, ie a combination of route number & operator  
-# stop_times <- unnest(stop_times)
+stop_times1 <- stop_times %>%  
+  unnest() %>%
+  left_join(stops, by = "stop_id") %>%
+  #st_as_sf() %>%
+  group_by(trip_id) %>%
+  nest(arrival_time, departure_time, stop_id, stop_sequence, pickup_type, drop_off_type)
+
 
   
 # make a shorter dataset
@@ -105,29 +83,24 @@ stop_times_supershort <- stop_times_short[c(25, 123, 500, 780, 1000, 2600, 5345,
     st_cast("LINESTRING") %>%
     left_join(st_drop_geometry(stop_times_supershort), by = "trip_id")
 
-    
-# # doesn't work 
-#      stop_times_supershort1 <- stop_times_supershort %>%
-#       filter(trip_id == min(trip_id)) %>%
-#       map(nest(geometry))
-#       st_linestring(geometry)
-#       st_multilinestring()
+# route_id - route plus direction plus operator
+  # service_id - route plus date/ day on which it's offered
 
-#   stop_times_supershort <- stop_times_supershort %>%
-#     nest(-(trip_id:route_type))
-# 
-#  
-# stop_times_supershort <- st_multilinestring(stop_times_supershort)
-# 
-# stop_times_supershort1 <- stop_times_supershort %>%
-#   filter(trip_id == min(trip_id)) %>%
-#   select(trip_id, stop_id, stop_name, geometry) %>%
-#   st_cast(geometry, to = "LINESTRING")
+  
+# make routes into a linestring  
+  
+  stop_times_short <- unnest(stop_times_supershort) %>%
+    left_join(stops, by = "stop_id") %>%
+    st_as_sf() %>%
+    group_by(trip_id) %>%
+    summarise(do_union = FALSE) %>%
+    st_cast("LINESTRING") %>%
+    left_join(st_drop_geometry(stop_times_supershort), by = "trip_id")    
+
 
 
 ##
 
-ps = rbind(c(10, 10), c(10, 5), c(11,9), c(11,10))
   
 # map #######################
 
