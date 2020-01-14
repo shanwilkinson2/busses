24c970d62a0ceec16cfe947eb675bc35d7ea6963
Shhh <shanwilkinson2@users.noreply.github.com>
2020-01-14 21:28:33 +0000

    save 501 map

--- 05 map stops and routes.R
+++ 05 map stops and routes.R
@@ -7,6 +7,7 @@
   library(RColorBrewer)
   library(glue)
   library(shinydashboard)
+  library(htmlwidgets)
 
 # read in files
   stops <- st_read("stops_extradata.geojson")
@@ -30,24 +31,29 @@
     filter(route_id == "NOR: 575:O:") # 42 stops
   stops_routes_short <- stops_routes %>%
     filter(route_id == "GTB: 575:O:") # 27 stops
+  stops_routes_short <- stops_routes %>%
+    filter(route_id == "GMN: 501:O:")
+  
   
   # join
   stops_routes_short <- 
     left_join(stops_routes_short, stops_life_exp, by = "stop_id")
 
-# map male life expectancy along route selected above
+# map life expectancy along route selected above
   
-  pal <- colorNumeric(palette = "BuPu", domain = stops_routes_short$male_life_exp)
+  pal <- colorNumeric(palette = "YlOrRd", domain = stops_routes_short$female_life_exp)
   mylabel <- glue("route number: {stops_routes_short$route_short_name} operator: {stops_routes_short$agency_id}<br>
                   {stops_routes_short$stop_name}<br>
-                  Male life expectancy: {stops_routes_short$male_life_exp}")
+                  Female life expectancy: {stops_routes_short$female_life_exp}")
   
-  leaflet(stops_routes_short) %>%  
+life_exp_map <-  leaflet(stops_routes_short) %>%  
     addProviderTiles("Stamen.TonerLite") %>%
-    addCircleMarkers(radius = 5, fillColor = ~pal(male_life_exp), 
-    popup = ~mylabel, weight = 2, fillOpacity = 0.8, color = "black")
+    addCircleMarkers(radius = 5, fillColor = ~pal(female_life_exp), 
+    popup = ~mylabel, weight = 2, fillOpacity = 0.8, color = "black") 
+
+saveWidget(life_exp_map, "life_exp_map.html")
   
-# shiny dashboard ############
+############# shiny dashboard ############
   
   ui <- dashboardPage(
     dashboardHeader(),
