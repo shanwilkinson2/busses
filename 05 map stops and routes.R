28e0753f59ac0818e7c7f6af794b40b0d2b2682f
Shhh <shanwilkinson2@users.noreply.github.com>
2020-01-22 22:24:39 +0000

    shiny dashboard got a bit interactive with a table

--- 05 map stops and routes.R
+++ 05 map stops and routes.R
@@ -8,6 +8,7 @@
   library(glue)
   library(shinydashboard)
   library(htmlwidgets)
+  library(shiny)
 
 # read in files
   stops <- st_read("stops_extradata.geojson")
@@ -25,7 +26,7 @@
   stops_life_exp <- stops %>%
     st_drop_geometry() %>%
     select(stop_id, male_life_exp, female_life_exp)
-  
+
   # test one route only
   stops_routes_short <- stops_routes %>%
     filter(route_id == "NOR: 575:O:") # 42 stops
@@ -38,10 +39,14 @@
   # join
   stops_routes_short <- 
     left_join(stops_routes_short, stops_life_exp, by = "stop_id")
+  
+  stops_routes_joined <- 
+    left_join(stops_routes, stops_life_exp, by = "stop_id")
 
 # map life expectancy along route selected above
+  # palette reversed so dark = low to emphasise low rather than high 
   
-  pal <- colorNumeric(palette = "YlOrRd", domain = stops_routes_short$female_life_exp)
+  pal <- colorNumeric(palette = "YlOrRd", domain = stops_routes_short$female_life_exp, reverse = TRUE)
   mylabel <- glue("route number: {stops_routes_short$route_short_name} operator: {stops_routes_short$agency_id}<br>
                   {stops_routes_short$stop_name}<br>
                   Female life expectancy: {stops_routes_short$female_life_exp}")
@@ -54,30 +59,44 @@ life_exp_map <-  leaflet(stops_routes_short) %>%
 saveWidget(life_exp_map, "life_exp_map.html")
   
 ############# shiny dashboard ############
-  
+
   ui <- dashboardPage(
     dashboardHeader(),
     dashboardSidebar(
-      selectInput("select_gender", "Gender:", 
+      varSelectInput("select_gender", "Gender:", 
+                     data = st_drop_geometry(stops_routes_short) %>%
+                       select(male_life_exp, female_life_exp),
                   c("Male" = "male_life_exp", "Female" = "female_life_exp")),
       selectInput("select_route_id", "Route ID", unique(stops_routes$route_id))
     ),
     dashboardBody(
-      leafletOutput("mymap")
+      h1("Life expectancy along a bus route"),
+      leafletOutput("mymap"),
+      tableOutput("table")
     )
   )
   
   server <- function(input, output) {
-    pal <- colorNumeric(palette = "BuPu", domain = stops_routes_short$male_life_exp)
-    mylabel <- glue("route number: {stops_routes_short$route_short_name} operator: {stops_routes_short$agency_id}<br>
-                  {stops_routes_short$stop_name}<br>
-                  Male life expectancy: {stops_routes_short$male_life_exp}")
-    
-    mymap <- leaflet(stops_routes_short) %>%  
-      addProviderTiles("Stamen.TonerLite") %>%
-      addCircleMarkers(radius = 5, fillColor = ~pal(male_life_exp), 
-                       popup = ~mylabel, weight = 2, fillOpacity = 0.8, color = "black")
-    output$mymap <- renderLeaflet(mymap)
+    # map
+      pal <- colorNumeric(palette = "BuPu", domain = stops_routes_short$male_life_exp, reverse = TRUE)
+      mylabel <- glue("route number: {stops_routes_short$route_short_name} operator: {stops_routes_short$agency_id}<br>
+                    {stops_routes_short$stop_name}<br>
+                    Male life expectancy: {stops_routes_short$male_life_exp}")
+      
+      mymap <- leaflet(stops_routes_short) %>%  
+        addProviderTiles("Stamen.TonerLite") %>%
+        addCircleMarkers(radius = 8, fillColor = ~pal(stops_routes_short$male_life_exp), 
+                         popup = ~mylabel, weight = 2, fillOpacity = 0.8, color = "black")
+      output$mymap <- renderLeaflet(mymap)
+      
+    # table
+      output$table <- renderTable(
+        stops_routes_joined %>%
+        st_drop_geometry() %>%
+        filter(route_id == input$select_route_id) %>%
+          select(route_short_name, route_long_name, stop_name, !!!input$select_gender) %>%
+          arrange(!!!input$select_gender)
+      )
   }
   
   shinyApp(ui, server)
